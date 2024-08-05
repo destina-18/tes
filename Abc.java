93bcf4a5de5294514bdf524bc31345b1f23e976b
destina-18 <destina_sari_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:41:10 +0700

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,13 @@
+public class Abc {
+    public static void main(String[] args) {
+        String nama = "destina";
+        int usia = 15;
+        String alamat = "jl. Danau Buyan Sawojajar";
+        double kelas = 10.7;
+
+        System.out.println("nama saya" + nama); 
+        System.out.println("Usia : "+ usia + "tahun");
+        System.out.println("Alamat : " + alamat);
+        System.out.println("kelas : "+ kelas);
+    }
+}
